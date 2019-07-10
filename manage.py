4c79bf458af113fce18217f13ca20c5e9910317f
ombaejr <ombaejr@gmail.com>
2019-07-10 05:00:12 +0300

    Manger shell decorator function and run command

--- manage.py
+++ manage.py
@@ -10,3 +10,18 @@ manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Blog = Blog,Comments = Comments )
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
