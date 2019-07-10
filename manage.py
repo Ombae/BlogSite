597bc2e95f32a942e8c5611084ce32bf6a211029
ombaejr <ombaejr@gmail.com>
2019-07-10 04:58:24 +0300

    Include imports and db commands in manage file

---
+++ manage.py
@@ -0,0 +1,12 @@
+from app import create_app,db
+from app.models import User,Blog,Comments
+from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
