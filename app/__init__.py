1cf6a842f1177fe2085af2a2680af437d9af99a8
ombaejr <ombaejr@gmail.com>
2019-07-10 05:14:15 +0300

    Write configs for the app and db

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,35 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 bcrypt = Bcrypt()
+# mail = Mail()
+
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    bootstrap.init_app(app)
+    bcrypt.init_app(app)
+    # mail.init_app(app)
+
+    # Creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
