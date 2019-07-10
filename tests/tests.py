7ddee8fc0c000337f9b7c537622846bc5f110ba1
ombaejr <ombaejr@gmail.com>
2019-07-10 07:18:46 +0300

    Make changes to correct tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,7 @@ from app import db
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User(password = 'banana', username = "john",email = "Test@me.com",profile_pic_path = "/srtatic/photos")
+        self.new_user = User(password = 'banana', username = "Seth",email = "seth@ms.com",profile_pic_path = "/static/photos")
 
     def test_save_user(self):
         self.new_user.save_user()
@@ -13,8 +13,8 @@ class UserModelTest(unittest.TestCase):
 
 class BlogTest(unittest.TestCase):
     def setUp(self):
-        # self.user_James = User(id=1,username = 'James',password = 'potato', email = 'james@ms.com',bio="Time is an abstract")
-        self.new_blog = Blog(id=5,title='Review for pitches',content="content",date_posted = "21-4-2018"  )
+        # self.user_Seth = User(id=1,username = 'Seth',password = 'potato', email = 'seth@ms.com',bio="Time is an abstract")
+        self.new_blog = Blog(id=5,title='Review for pitches',content="content",date_posted = "10-7-2018"  )
 
     def tearDown(self):
         Blog.query.delete()
@@ -23,7 +23,7 @@ class BlogTest(unittest.TestCase):
     def test_check_instance_variables(self):
         self.assertEquals(self.new_blog.id,5)
         self.assertEquals(self.new_blog.title,'Review for pitches')
-        self.assertEquals(self.new_blog.date_posted,"21-4-2018")
+        self.assertEquals(self.new_blog.date_posted,"10-7-2018")
 
 
     def test_save_blog(self):
